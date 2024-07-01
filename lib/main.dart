afed52c41d5d72c2c047d1c89df947f9ad248b3f
wkwtfigo <e.zagurskih@innopolis.university>
2024-07-01 16:29:06 +0300

    update dark theme fonts

--- lib/main.dart
+++ lib/main.dart
@@ -41,7 +41,7 @@ class MyApp extends StatelessWidget {
       textTheme: TextTheme(
         bodyLarge: TextStyle(color: Colors.white),
         bodyMedium: TextStyle(color: Colors.white),
-        bodySmall: TextStyle(color: Colors.white),
+        //bodySmall: TextStyle(color: Colors.white),
         titleMedium: TextStyle(color: Colors.white),
         titleSmall: TextStyle(color: Colors.white),
         titleLarge: TextStyle(color: Colors.white),
