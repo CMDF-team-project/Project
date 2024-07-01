cb37b8462d6b12d6282ed039c259c3becd66f97e
olesia8novoselova <nov-olesya.m@inbox.ru>
2024-07-01 13:02:23 +0300

    update

--- team_project/lib/main.dart
+++ team_project/lib/main.dart
@@ -7,6 +7,9 @@ import 'package:team_project/storage/provider.dart';
 import 'connection.dart';
 import 'package:connectivity_plus/connectivity_plus.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:intl/intl.dart';
+import 'l10n/l10n.dart';
 
 late final ConnectivityService connectionService;
 
@@ -31,8 +34,34 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SplashScreen(),
+    return MaterialApp(
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+        AppLocalizations.delegate,  // Custom AppLocalizations delegate
+      ],
+      supportedLocales: const [
+        Locale('en', 'US'), // English
+        Locale('ru', 'RU'), // Russian
+      ],
+      home: const SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(AppLocalizations.of(context)?.translate('title') ?? 'Loading...'),
+      ),
+      body: Center(
+        child: Text(AppLocalizations.of(context)?.translate('welcome') ?? 'Loading...'),
+      ),
     );
   }
 }
